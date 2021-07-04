d740baf830e1d7e6112a27a860961cee48a481a2
757299248 <32446021+757299248@users.noreply.github.com>
2021-07-04 09:39:23 +0800

    Update board.h

--- trunk/configs/boards/MT300N/board.h
+++ trunk/configs/boards/MT300N/board.h
@@ -1,28 +1,32 @@
-/* MT300N */
+/* GLI MT300N */
 
-#define BOARD_PID		"MT300N"
-#define BOARD_NAME		"MT300N"
-#define BOARD_DESC		"MT300N Wireless Router"
-#define BOARD_VENDOR_NAME	"MT300N WIFI"
-#define BOARD_VENDOR_URL	"http://www.5ksh.com/"
-#define BOARD_MODEL_URL		"http://www.5ksh.com/product"
-#define BOARD_BOOT_TIME		20
-#define BOARD_FLASH_TIME	120
-#define BOARD_GPIO_BTN_RESET	12
-#undef BOARD_GPIO_BTN_WPS
-#undef  BOARD_GPIO_LED_ALL
-#define BOARD_GPIO_LED_WIFI	72
-#undef BOARD_GPIO_LED_POWER
-#undef BOARD_GPIO_LED_LAN
-#undef BOARD_GPIO_LED_WAN
-#undef BOARD_GPIO_LED_USB
-#undef  BOARD_GPIO_LED_ROUTER
-#undef  BOARD_GPIO_PWR_USB
-#define BOARD_HAS_5G_11AC	0
-#define BOARD_NUM_ANT_5G_TX	0
-#define BOARD_NUM_ANT_5G_RX	0
-#define BOARD_NUM_ANT_2G_TX	2
-#define BOARD_NUM_ANT_2G_RX	2
-#define BOARD_NUM_ETH_LEDS	0
-#define BOARD_HAS_EPHY_L1000	0
-#define BOARD_HAS_EPHY_W1000	0
+#定义 BOARD_PID 		" GL-MT300N "
+#定义 BOARD_NAME 		" GL-MT300N "
+＃定义 BOARD_DESC 		“ GL技术MT300N路由器”
+#定义 BOARD_VENDOR_NAME 	" GL Technologies "
+#定义 BOARD_VENDOR_URL 	" https://www.gl-inet.com "
+#定义 BOARD_MODEL_URL 		" https://www.gl-inet.com/mt300n/ "
+#定义 BOARD_BOOT_TIME 		20
+#定义 BOARD_FLASH_TIME 	90
+#定义 BOARD_GPIO_BTN_RESET 	1
+# undef   BOARD_GPIO_BTN_WPS 2	 /*空闲引脚*/
+# undef   BOARD_GPIO_BTN_BT1 42	 /*向左切换*/
+# undef   BOARD_GPIO_BTN_BT2 43	 /*向右切换*/
+# undef   BOARD_GPIO_LED_ALL
+#定义 BOARD_GPIO_LED_WIFI 	72 	/*前面编号 3 */
+#定义 BOARD_GPIO_LED_POWER 	41 	/*前面编号 1 */
+#定义 BOARD_GPIO_LED_LAN 	40 	/*前面编号 2 */
+# undef   BOARD_GPIO_LED_WAN
+# undef   BOARD_GPIO_LED_USB
+# undef   BOARD_GPIO_LED_ROUTER
+#定义 BOARD_GPIO_PWR_USB 	26
+＃限定 BOARD_GPIO_PWR_USB_ON 	1 	/ * 1：5V电源ON，0：5V电源OFF * /
+#定义 BOARD_HAS_5G_11AC 	0
+#定义 BOARD_NUM_ANT_5G_TX 	0
+#定义 BOARD_NUM_ANT_5G_RX 	0
+#定义 BOARD_NUM_ANT_2G_TX 	2
+#定义 BOARD_NUM_ANT_2G_RX 	2
+#定义 BOARD_NUM_ETH_LEDS 	0
+#定义 BOARD_NUM_ETH_EPHY 	2
+#定义 BOARD_HAS_EPHY_L1000 	0
+#定义 BOARD_HAS_EPHY_W1000 	0
